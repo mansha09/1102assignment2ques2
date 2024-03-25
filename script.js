c5d7fe64d3e53153661fcdc87957d7d7b80a8429
mansha09 <144940108+mansha09@users.noreply.github.com>
2024-03-24 18:45:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+$(document).ready(function() {
+    // Handle click event for the BACK button
+    $('#backBtn').click(function() {
+      // Navigate to the Blog page
+      window.location.href = 'Blog.html';
+    });
+  });
+
+
+
+ 
+  
+  
